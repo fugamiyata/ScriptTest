05e24d13f02bb21a29c079e131456035f7d82185
fugamiyata <72694228+fugamiyata@users.noreply.github.com>
2020-11-06 15:40:23 +0900

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Test : MonoBehaviour
+{
+    // Use this for initialization
+    void Start ()
+    {
+        // 配列を初期化する
+        int[] array = new int[5];
+        // 配列の各要素に値を代入する
+
+        array[0] = 20;
+        array[1] = 90;
+        array[2] = 120;
+        array[3] = 30;
+        array[4] = 60;
+        // 配列の要素をすべて表示する
+
+        for (int i = 0; i < 5; i++)
+        {
+          
+            Debug.Log(array[i]);
+
+        }
+        for (int i = 4; i >= 0; i--)
+        {
+            Debug.Log(array[i]);
+
+        }
+
+    }
+    // Update is called once per frame
+    
+    void Update ()
+    {
+
+    }
+
+
+}
+
